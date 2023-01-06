fd704ba216d7a96224ce839906cd67b34384ba69
koji <kk2796@nyu.edu>
2023-01-06 00:32:08 -0500

    2nd stage code

--- app/main.js
+++ app/main.js
@@ -3,9 +3,17 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
+const convertValue = (input) => {
+  return `{input}\r\n`
+}
+
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   //   // Handle connection
+  connection.on('data', (data) => {
+    const returnValue = convertValue(data)
+    connection.writable(returnValue)
+  })
 })
 //
 server.listen(6379, '127.0.0.1')
