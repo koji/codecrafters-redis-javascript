209547f1ab9b81a99384892ef596abc9f9fbf854
koji <kk2796@nyu.edu>
2023-01-15 13:47:37 -0500

    modify parseInput

--- app/main.js
+++ app/main.js
@@ -37,7 +37,7 @@ const server = net.createServer((connection) => {
 
       case 'get':
         const storedKey = store[key] ? `+${store[key]}\r\n` : '$-1\r\n'
-        connection.write(storedKey)
+        connection.write(connection.write(storedKey))
         return
 
       case 'pong':
