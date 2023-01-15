c57027094a196cfce916ad7d90a9ff76706934a5
koji <kk2796@nyu.edu>
2023-01-15 13:44:46 -0500

    add storedKey

--- app/main.js
+++ app/main.js
@@ -3,6 +3,8 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
+let store = {}
+
 const convertValue = (input) => {
   const value = input.toString()
   return `+${value}\r\n`
